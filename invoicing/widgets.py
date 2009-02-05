6f22cd7bb59617e018d8138f5e92dbedb982e823
Will Roe <willroe@gmail.com>
2009-02-05 23:57:00 +0000

    Making the client form sane

--- invoicing/widgets.py
+++ invoicing/widgets.py
@@ -1,16 +1,21 @@
 from turbogears.widgets import forms
 from turbogears import validators
+from invoicing.model import ClientGroup
 
 class ClientFields(forms.WidgetsList):
     """Form to create a client"""
 
-    #def __init__(self, groups):
     name = forms.TextField(validator=validators.NotEmpty())
-    abreveated = forms.TextField(validator=validators.NotEmpty(), attrs={'size':2})
+    abbreveated = forms.TextField(validator=validators.NotEmpty(), attrs={'size':2})
     address = forms.TextArea(validator=validators.NotEmpty())
     country = forms.TextField(validator=validators.NotEmpty())
-    vat_number = forms.TextField(validator=validators.NotEmpty())
-    email_address = forms.TextField(validator=validators.Email(not_empty=True))
-        #self.group = forms.SingleSelectField(options=[(g.id, g.name) for g in groups])
+    vat_number = forms.TextField(validator=validators.NotEmpty(),label="VAT number")
+    email_address = forms.TextField(validator=validators.Email(not_empty=True),label="Email address")
+    client_group = forms.SingleSelectField(validator=validators.NotEmpty(),
+                                           label="Client Group",
+                                           options=[(g.id, g.name) for g in ClientGroup.all_client_groups().all()])
 
-
+client_form = forms.TableForm(
+    fields=ClientFields(),
+    action="save_client"
+    )
