b12ec794565f88df5d5e4d0b9c1c40e380134f2c
Will Roe <willroe@gmail.com>
2009-02-05 23:56:16 +0000

    Adding Invoice previous/next

--- invoicing/model.py
+++ invoicing/model.py
@@ -6,7 +6,7 @@ pkg_resources.require("SQLAlchemy>=0.3.10")
 pkg_resources.require("Elixir>=0.4.0")
 # import the basic Elixir classes and functions for declaring the data model
 # (see http://elixir.ematia.de/trac/wiki/TutorialDivingIn)
-from elixir import Entity, Field, OneToMany, ManyToOne, ManyToMany, ColumnProperty
+from elixir import Entity, Field, OneToMany, ManyToOne, ManyToMany, ColumnProperty, OneToOne
 from elixir import options_defaults, using_options, setup_all
 # import some datatypes for table columns from Elixir
 # (see http://www.sqlalchemy.org/docs/04/types.html for more)
@@ -22,26 +22,29 @@ class Invoice(Entity):
     ident = Field(String, unique=True)
     created = Field(DateTime, default=datetime.now)
     date = Field(DateTime, nullable=False)
+    date_sent = Field(DateTime)
     paid = Field(DateTime, default=None)
     terms = Field(String, default="30 days")
     status = Field(Enum(["Proforma","Invoice","Calcelled"])) # Don't know about Enum yet, might have to use own type
     client = ManyToOne('Client') # and OneToMany('Invoice') in Client class
     vat_rate = Field(Numeric(precision=3, scale=1)) # scale?
     vat = Field(Numeric)
+    next_invoice = OneToOne('Invoice', inverse='previous_invoice')
+    previous_invoice = ManyToOne('Invoice', inverse='next_invoice')
 
     products = ManyToMany('Product')
 
 class Client(Entity):
     using_options(tablename="client")
     name = Field(String, unique=True)
-    abreveated = Field(String(2), unique=True)
+    abbreveated = Field(String(2), unique=True)
     address = Field(Unicode)
     country = Field(Unicode)
     vat_number = Field(String)
     email_address = Field(Unicode(255), unique=True)
     invoices = OneToMany('Invoice')
     group = ManyToOne('ClientGroup')
-    full_address = ColumnProperty(lambda c: c.name + c.abreveated)
+    full_address = ColumnProperty(lambda c: c.name + c.abbreveated)
 
     @property
     def invoices_this_year(self):
@@ -57,7 +60,7 @@ class Client(Entity):
     @property
     def next_invoice_ident(self):
         num = self.invoices_this_year.count() + 1
-        return "-".join(map(str,[self.abreveated, datetime.today().year, "%02i" % num]))
+        return "-".join(map(str,[self.abbreveated, datetime.today().year, "%02i" % num]))
 
 class Product(Entity):
     using_options(tablename="product")
@@ -86,6 +89,10 @@ class ClientGroup(Entity):
     name = Field(Unicode, unique=True)
     company = ManyToOne('Company')
     clients = OneToMany('Client')
+
+    def getClientGroups(self):
+        return ClientGroup.query()
+    all_client_groups = classmethod(getClientGroups)
 
 class Visit(Entity):
     """
