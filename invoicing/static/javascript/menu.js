60917315d80623da25c8963d7ffcfac8d88a3859
Will Roe <willroe@gmail.com>
2009-02-06 15:24:40 +0000

    Added debug() function to control debug messages - relies on logDebug() from MochiKit

--- invoicing/static/javascript/menu.js
+++ invoicing/static/javascript/menu.js
@@ -1,28 +1,36 @@
+//TODO: Ensure this code executes WITHOUT console.log...
+
 var headers = [];
 var submenus = [];
 
-function expandSubmenu(event) {
+function debug() {
+    if (typeof(console) != 'undefined') {
+	logDebug.apply(this, arguments);
+    }
+}
+
+/*function expandSubmenu(event) {
     event.preventDefault();
     var target = event.target();
-    console.log("Event: ", event.target());
+    debug("Event: ", event.target());
     
     
 }
 
 function menuHeader(header) {
-    console.log("H4: ", header);
+    debug("H4: ", header);
     var a_link = getFirstElementByTagAndClassName("a", null, header);
-    console.log("a_links: ", a_link);
+    debug("a_links: ", a_link);
     connect(a_link, "onclick", expandSubmenu);
-}
+}*/
 
 function menuConnector(idx) {
     var a_link = getFirstElementByTagAndClassName("a", null, headers[idx]);
-    console.log("a_links: ", a_link);
+    debug("a_links: ", a_link);
     connect(a_link, "onclick", partial(function(idx,event) {
 	event.preventDefault();
 	var target = event.target();
-	console.log("Event: ", event.target());
+	debug("Event: ", event.target());
 	for (var sub in submenus) {
 	    if (idx == sub)
 		slideDown(submenus[sub]);
@@ -32,34 +40,34 @@ function menuConnector(idx) {
     },idx));
 }
 
-function connectLink(header) {
-    console.log("Header: ", header);
+/*function connectLink(header) {
+    debug("Header: ", header);
     var div_parent = getFirstParentByTagAndClassName(header, "div");
-    console.log("DivParent: ", div_parent);
+    debug("DivParent: ", div_parent);
     headers = getElementsByTagAndClassName("h4", null, div_parent);
     //map(menuHeader, headers);
-    console.log("There are ", headers.length, " headers");
-    console.log("There are ", submenus.length, " submenus");
+    debug("There are ", headers.length, " headers");
+    debug("There are ", submenus.length, " submenus");
     if (headers.length == submenus.length) {
 	for (var h_idx in headers) {
 	    menuConnector(h_idx);
 	}
     } else {
-	console.log("There are different numbers of headers to submenus");
+	debug("There are different numbers of headers to submenus");
     }
-}
+}*/
 
 function initMenu() {
     headers = getElementsByTagAndClassName("h4", "menuheader", $('menu'));
     submenus = getElementsByTagAndClassName("ul", "submenu", $('menu'));
-    console.log("There are ", headers.length, " headers");
-    console.log("There are ", submenus.length, " submenus");
+    debug("There are ", headers.length, " headers");
+    debug("There are ", submenus.length, " submenus");
     if (headers.length == submenus.length) {
 	for (var h_idx in headers) {
 	    menuConnector(h_idx);
 	}
     } else {
-	console.log("There are different numbers of headers to submenus");
+	debug("There are different numbers of headers to submenus");
     }
     map(hideElement, submenus);
     //map(connectLink, headers);
