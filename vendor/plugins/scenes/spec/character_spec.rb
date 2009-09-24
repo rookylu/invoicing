6aee1bb3b337e04f1f3713b9ab6334c837751285
William Roe <william.roe@migcan.com>
2009-09-24 22:52:12 +0100

    Braid: Added mirror 'vendor/plugins/scenes' at 'cc0c092'

---
+++ vendor/plugins/scenes/spec/character_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Scenes::Character do
+
+  it "should add new cast members"
+
+  it "should be able to clear the list of class members"
+
+  it "should show a list of cast members"
+
+  it "should be able to duplicate itself"
+
+  it "should be able to update itself"
+
+  it "should fetch itself from the database if it is an ActiveRecord object and it has already been called"
+
+  it "should return itself if not an ActiveRecord object"
+
+  it "should ignore case when referencing a cast member"
+
+end
